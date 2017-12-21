19a1c10873aec54195ae2bffcf33a63d21f3cbe8
JeromeJ <jerome.jamart.42@gmail.com>
2017-12-21 17:27:35 +0100

    First commit

---
+++ Assets/_Android/Scripts/AndroidExplorer.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AndroidExplorer : MonoBehaviour
+{
+    #region Public Members
+
+    public Text debug;
+    public GameObject player;
+
+    #endregion
+
+    #region Public void
+
+    #endregion
+
+    #region System
+
+    protected void Awake()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    //public void GetTouchCoụnt
+
+    private void Update()
+    {
+        debug.text = "";
+
+        debug.text += "Touch count: " + Input.touchCount + "\n";
+
+        if (Input.touchCount > 0)
+        {
+            debug.text += "Position: " + Input.touches[0].position + "\n";
+        }
+
+        
+
+        debug.text += "\n";
+
+        debug.text += "Acceleration: " + Input.acceleration+ "\n";
+        debug.text += "Compass:" + Input.compass.rawVector + "\n";
+
+        debug.text += "\n";
+
+        // Needs to be enabled first!!
+        debug.text += "Gyro: " + Input.gyro.userAcceleration + "\n";
+        debug.text += "Gyro attitude: " + Input.gyro.attitude+ "\n";
+
+        debug.text += "\n";
+
+        debug.text += "Longitutde: " + Input.location.lastData.longitude+ "\n";
+        debug.text += "Latitude: " + Input.location.lastData.latitude + "\n";
+
+        // Android asset toolkit (not free)
+        // Android + profiler + wifi = happiness
+
+        Debug.Log(player);
+
+        try
+        {
+            player.transform.Translate(player.GetComponentInChildren<Camera>().transform.forward * (Input.touchCount) * 0.7f);
+        }
+        catch (System.Exception e)
+        {
+            debug.text += e;
+        }
+    }
+
+    #endregion
+
+    #region Class Methods
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    #endregion
+}
